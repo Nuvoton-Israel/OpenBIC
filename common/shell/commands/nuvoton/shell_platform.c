b3b1d0c67b5517dc326f0a9b2a5d0075df1375af
cpchiang <cpchiang1@nuvoton.com>
2023-07-18 19:35:57 -0700

    yv35-npcm-test: add nuvoton flash shell test items
    add nuvoton flash shell test items
    
    1. add re_init and sfpd_read in platform flash test.
    
    Signed-off-by: cpchiang <cpchiang1@nuvoton.com>

--- common/shell/commands/nuvoton/shell_platform.c
+++ common/shell/commands/nuvoton/shell_platform.c
@@ -16,11 +16,13 @@
 
 #include "commands/gpio_shell.h"
 #include "commands/info_shell.h"
+#include "commands/flash_shell.h"
 
 /* MAIN command */
 SHELL_STATIC_SUBCMD_SET_CREATE(
 	sub_platform_cmds, SHELL_CMD(info, NULL, "Platform info.", cmd_info_print),
 	SHELL_CMD(gpio, &sub_gpio_cmds, "GPIO relative command.", NULL),
+	SHELL_CMD(flash, &sub_flash_cmds, "FLASH(spi) relative command.", NULL),
 	SHELL_SUBCMD_SET_END);
 
 SHELL_CMD_REGISTER(platform, &sub_platform_cmds, "Platform commands", NULL);
