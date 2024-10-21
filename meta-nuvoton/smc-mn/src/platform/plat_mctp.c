ea317107ec32cadcf9cf17a5dba23487b40f82ce
Joseph Liu <kwliu@nuvoton.com>
2024-10-21 15:13:37 +0800

    meta-nuvoton: add scm mn machine
    Signed-off-by: Joseph Liu <kwliu@nuvoton.com>

---
+++ meta-nuvoton/smc-mn/src/platform/plat_mctp.c
@@ -0,0 +1,241 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <sys/printk.h>
+#include <logging/log.h>
+#include <logging/log_ctrl.h>
+#include <stdlib.h>
+#include "libutil.h"
+#include "mctp.h"
+#include "mctp_ctrl.h"
+#include "pldm.h"
+#include "ipmi.h"
+#include "sensor.h"
+//#include "plat_hook.h"
+#include "plat_mctp.h"
+#include "plat_gpio.h"
+#include "plat_i2c.h"
+#include "cci.h"
+#include "pm8702.h"
+#include "util_sys.h"
+
+LOG_MODULE_REGISTER(plat_mctp);
+
+
+static mctp_port plat_mctp_port[] = {
+	 /*{ .channel_target = PLDM,
+		.conf.i3c_conf.addr = I3C_STATIC_ADDR_BMC,
+          .conf.i3c_conf.bus = I3C_BUS_BMC,
+          .medium_type = MCTP_MEDIUM_TYPE_TARGET_I3C },
+       { .conf.smbus_conf.addr = I2C_ADDR_BIC,
+          .conf.smbus_conf.bus = I2C_BUS_CXL,
+          .medium_type = MCTP_MEDIUM_TYPE_SMBUS },
+        { .conf.smbus_conf.addr = I2C_ADDR_BIC,
+          .conf.smbus_conf.bus = I2C_BUS_PLDM,
+          .medium_type = MCTP_MEDIUM_TYPE_SMBUS },
+	{ .conf.smbus_conf.addr = 0,
+	  .conf.smbus_conf.bus = 0,
+	  .medium_type = MCTP_MEDIUM_TYPE_SERIAL },*/
+	{
+		.channel_target = PLDM, 
+		.conf.usb_conf.addr = 0,
+	  .conf.usb_conf.bus = 0,
+	  .medium_type = MCTP_MEDIUM_TYPE_USB },
+/*	{ .conf.i3c_conf.addr = I3C_STATIC_ADDR_BIC,
+	  .conf.i3c_conf.bus = I3C_BUS_MASTER,
+	  .medium_type = MCTP_MEDIUM_TYPE_CONTROLLER_I3C },*/
+};
+
+static mctp_route_entry plat_mctp_route_tbl[] = {
+	/*{ MCTP_EID_CXL_I2C, I2C_BUS_CXL, I2C_ADDR_CXL0, .set_endpoint = false},
+	{ MCTP_EID_BMC_I2C, I2C_BUS_PLDM, I2C_ADDR_BMC, .set_endpoint = false},
+	{ MCTP_EID_BIC_I2C, I2C_BUS_PLDM, I2C_ADDR_BIC, .set_endpoint = false},
+	{ MCTP_EID_BIC_I3C, I3C_BUS_MASTER, I3C_STATIC_ADDR_BIC, .set_endpoint = false},
+	{ MCTP_EID_BMC_I3C, I3C_BUS_BMC, I3C_STATIC_ADDR_BMC, .set_endpoint = false},*/
+	{ 0xa0, 0x0, 0x0, .set_endpoint = false},
+};
+
+mctp *find_mctp_by_bus(uint8_t bus)
+{
+	uint8_t i;
+	for (i = 0; i < ARRAY_SIZE(plat_mctp_port); i++) {
+		mctp_port *p = plat_mctp_port + i;
+
+		if (p->medium_type == MCTP_MEDIUM_TYPE_SMBUS) {
+			if (bus == p->conf.smbus_conf.bus) {
+				return p->mctp_inst;
+			}
+		} else if (p->medium_type == MCTP_MEDIUM_TYPE_TARGET_I3C) {
+			if (bus == p->conf.i3c_conf.bus) {
+				return p->mctp_inst;
+			}
+		} else if (p->medium_type == MCTP_MEDIUM_TYPE_CONTROLLER_I3C) {
+			if (bus == p->conf.i3c_conf.bus) {
+				return p->mctp_inst;
+			}
+		} else if (p->medium_type == MCTP_MEDIUM_TYPE_USB) {
+			if (bus == p->conf.usb_conf.bus) {
+				return p->mctp_inst;
+			}
+		} else {
+			LOG_ERR("Unknown medium type:0x%x\n", p->medium_type);
+			return NULL;
+		}
+	}
+
+	return NULL;
+}
+
+static uint8_t mctp_msg_recv(void *mctp_p, uint8_t *buf, uint32_t len, mctp_ext_params ext_params)
+{
+	CHECK_NULL_ARG_WITH_RETURN(mctp_p, MCTP_ERROR);
+	CHECK_NULL_ARG_WITH_RETURN(buf, MCTP_ERROR);
+
+	/* first byte is message type and ic */
+	uint8_t msg_type = (buf[0] & MCTP_MSG_TYPE_MASK) >> MCTP_MSG_TYPE_SHIFT;
+
+	switch (msg_type) {
+	case MCTP_MSG_TYPE_CTRL:
+		mctp_ctrl_cmd_handler(mctp_p, buf, len, ext_params);
+		break;
+
+	case MCTP_MSG_TYPE_PLDM:
+		mctp_pldm_cmd_handler(mctp_p, buf, len, ext_params);
+		break;
+
+	default:
+		LOG_WRN("Cannot find message receive function!!");
+		return MCTP_ERROR;
+	}
+
+	return MCTP_SUCCESS;
+}
+
+static uint8_t get_mctp_route_info(uint8_t dest_endpoint, void **mctp_inst,
+				   mctp_ext_params *ext_params)
+{
+	CHECK_NULL_ARG_WITH_RETURN(mctp_inst, MCTP_ERROR);
+	CHECK_NULL_ARG_WITH_RETURN(ext_params, MCTP_ERROR);
+
+	uint8_t rc = MCTP_ERROR;
+	uint32_t i;
+
+	for (i = 0; i < ARRAY_SIZE(plat_mctp_route_tbl); i++) {
+		mctp_route_entry *p = plat_mctp_route_tbl + i;
+		if (p->endpoint == dest_endpoint) {
+			*mctp_inst = find_mctp_by_bus(p->bus);
+
+			if (p->bus == 0) {
+				ext_params->type = MCTP_MEDIUM_TYPE_USB;
+				ext_params->usb_ext_params.dummy = p->addr;
+			} else if (p->bus == I3C_BUS_MASTER) {
+				ext_params->type = MCTP_MEDIUM_TYPE_CONTROLLER_I3C;
+				ext_params->i3c_ext_params.addr = p->addr;
+			} else if (p->bus != I3C_BUS_BMC) {
+				ext_params->type = MCTP_MEDIUM_TYPE_SMBUS;
+				ext_params->smbus_ext_params.addr = p->addr;
+			} else {
+				ext_params->type = MCTP_MEDIUM_TYPE_TARGET_I3C;
+				ext_params->i3c_ext_params.addr = p->addr;
+			}
+			rc = MCTP_SUCCESS;
+			break;
+		}
+	}
+
+	return rc;
+}
+
+uint8_t get_mctp_info(uint8_t dest_endpoint, mctp **mctp_inst, mctp_ext_params *ext_params)
+{
+	CHECK_NULL_ARG_WITH_RETURN(mctp_inst, MCTP_ERROR);
+	CHECK_NULL_ARG_WITH_RETURN(ext_params, MCTP_ERROR);
+
+	uint8_t rc = MCTP_ERROR;
+	uint32_t i;
+
+	for (i = 0; i < ARRAY_SIZE(plat_mctp_route_tbl); i++) {
+		mctp_route_entry *p = plat_mctp_route_tbl + i;
+		if (p->endpoint == dest_endpoint) {
+			*mctp_inst = find_mctp_by_bus(p->bus);
+			if (p->bus == 0x0) {
+				ext_params->type = MCTP_MEDIUM_TYPE_USB;
+				ext_params->usb_ext_params.dummy = p->addr;
+			} else if (p->bus != I3C_BUS_BMC) {
+				ext_params->type = MCTP_MEDIUM_TYPE_SMBUS;
+				ext_params->smbus_ext_params.addr = p->addr;
+			} else {
+				ext_params->type = MCTP_MEDIUM_TYPE_TARGET_I3C;
+				ext_params->i3c_ext_params.addr = p->addr;
+			}
+			ext_params->ep = p->endpoint;
+			rc = MCTP_SUCCESS;
+			break;
+		}
+	}
+
+	return rc;
+}
+
+void plat_mctp_init()
+{
+	int ret = 0;
+
+	/* init the mctp/pldm instance */
+	for (uint8_t i = 0; i < ARRAY_SIZE(plat_mctp_port); i++) {
+		mctp_port *p = plat_mctp_port + i;
+
+		p->mctp_inst = mctp_init();
+		if (!p->mctp_inst) {
+			LOG_ERR("mctp_init failed!!");
+			continue;
+		}
+
+		uint8_t rc = mctp_set_medium_configure(p->mctp_inst, p->medium_type, p->conf);
+		if (rc != MCTP_SUCCESS) {
+			LOG_ERR("mctp set medium configure failed");
+		}
+
+		mctp_reg_endpoint_resolve_func(p->mctp_inst, get_mctp_route_info);
+
+		mctp_reg_msg_rx_func(p->mctp_inst, mctp_msg_recv);
+
+		ret = mctp_start(p->mctp_inst);
+	}
+}
+
+uint8_t plat_get_mctp_port_count()
+{
+	return ARRAY_SIZE(plat_mctp_port);
+}
+
+mctp_port *plat_get_mctp_port(uint8_t index)
+{
+	return plat_mctp_port + index;
+}
+
+
+uint8_t pal_get_bmc_interface()
+{
+	return BMC_INTERFACE_USB;
+}
