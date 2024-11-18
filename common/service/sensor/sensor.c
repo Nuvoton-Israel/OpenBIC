7996d761cb2f213ed71b3e148256101019bf5759
Joseph Liu <kwliu@nuvoton.com>
2024-11-18 12:09:43 +0800

    smc-mn: enable pldm sensors
    add pldm adc sensor
    
    Signed-off-by: Joseph Liu <kwliu@nuvoton.com>

--- common/service/sensor/sensor.c
+++ common/service/sensor/sensor.c
@@ -21,6 +21,7 @@
 #include <string.h>
 #include <logging/log.h>
 #include "power_status.h"
+#include "npcm4xx_adc.h"
 #include "sdr.h"
 #include "pdr.h"
 #include "hal_i2c.h"
@@ -145,6 +146,7 @@ const char *const sensor_type_name[] = {
 	sensor_name_to_num(bmr4922302_803)
 	sensor_name_to_num(emc1413)
 	sensor_name_to_num(bcm85658)
+	sensor_name_to_num(npcm4xx_adc)
 };
 // clang-format on
 
@@ -363,6 +365,8 @@ SENSOR_DRIVE_INIT_DECLARE(emc1413);
 #ifdef ENABLE_BCM85658
 SENSOR_DRIVE_INIT_DECLARE(bcm85658);
 #endif
+
+SENSOR_DRIVE_INIT_DECLARE(npcm4xx_adc);
 
 // The sequence needs to same with SENSOR_DEV ID
 sensor_drive_api sensor_drive_tbl[] = {
@@ -729,6 +733,7 @@ sensor_drive_api sensor_drive_tbl[] = {
 #else
 	SENSOR_DRIVE_TYPE_UNUSE(bcm85658),
 #endif
+	SENSOR_DRIVE_TYPE_INIT_MAP(npcm4xx_adc),
 };
 
 static void init_sensor_num(void)
